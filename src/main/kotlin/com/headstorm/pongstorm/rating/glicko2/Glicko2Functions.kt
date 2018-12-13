a18676c15ba5b1c8a0525c862028816555975533
Challen Herzberg-Brovold <crnhb38@gmail.com>
2018-12-13 17:58:48 -0600

    Adding iterative fn

--- src/main/kotlin/com/headstorm/pongstorm/rating/glicko2/Glicko2Functions.kt
+++ src/main/kotlin/com/headstorm/pongstorm/rating/glicko2/Glicko2Functions.kt
@@ -4,6 +4,8 @@ import kotlin.math.PI
 import kotlin.math.exp
 import kotlin.math.pow
 
+val epsilon = 0.000001
+
 // This function will take glicko2 scores, not glicko scores
 fun calculatePlayerScore(player: Player, results: List<Result>): Player {
 
@@ -34,6 +36,16 @@ fun deltaSumFunction(player: Player, result: Result): Float {
     val eValue = eFunction(playerRating, oppRating, oppDeviation)
 
     return gValue * (result.type.score - eValue)
+}
+
+fun volatilityIterativeFn(deviation: Float, v: Float, delta: Float, tao: Float): (Float, Float) -> Float = { x, a ->
+    val num1 = exp(x) * (delta.pow(2) - deviation.pow(2) - v - exp(x))
+    val den1 = 2 * (deviation.pow(2) + v + exp(x)).pow(2)
+
+    val num2 = x - a
+    val den2 = tao.pow(2)
+
+     num1 / den1 - num2 / den2
 }
 
 fun eFunction(rating: Float, oppRating: Float, oppDeviation: Float): Float {
