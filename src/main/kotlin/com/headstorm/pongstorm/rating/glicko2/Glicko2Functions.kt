8f8502ea37dd28957b064d9b62e829ffb1f1a20b
Challen Herzberg-Brovold <crnhb38@gmail.com>
2018-12-13 17:04:51 -0600

    Adding some more functions

--- src/main/kotlin/com/headstorm/pongstorm/rating/glicko2/Glicko2Functions.kt
+++ src/main/kotlin/com/headstorm/pongstorm/rating/glicko2/Glicko2Functions.kt
@@ -5,35 +5,38 @@ import kotlin.math.exp
 import kotlin.math.pow
 
 // This function will take glicko2 scores, not glicko scores
-// For this reason, we will not do the conversion
 fun calculatePlayerScore(player: Player, results: List<Result>): Player {
 
 
-
+    val v = results.map{ vSumFunction(player, it) }.reduce{ acc, it -> acc + it }.pow(-1)
+    val delta = results.map{ deltaSumFunction(player, it) }.reduce{ acc, it -> acc + it }.times(v)
     return Player(123, 1f, 1f, 1f)
 }
 
-// Computes the sum
-fun computeVSigma(player: Player, results: List<Result>): Float {
-
-    // Using inline function looks less bad than function reference
-    val oneOverVSum = results.map{ vSummedFunction(player, it) }.reduce{ acc, it -> acc + it}
-    return oneOverVSum.pow(-1)
-}
-
 // Function to be called in summation
-fun vSummedFunction(player: Player, result: Result): Float {
+fun vSumFunction(player: Player, result: Result): Float {
     val playerRating = player.rating
     val oppRating = result.opponent.rating
     val oppDeviation = result.opponent.deviation
 
     val gValue = gFunction(oppDeviation)
-    val eValue = eFunction(playerRating, oppRating, oppDeviation, result.type.score)
+    val eValue = eFunction(playerRating, oppRating, oppDeviation)
 
     return gValue.pow(2) * eValue * (1 - eValue)
 }
 
-fun eFunction(rating: Float, oppRating: Float, oppDeviation: Float, score: Float): Float {
+fun deltaSumFunction(player: Player, result: Result): Float {
+    val playerRating = player.rating
+    val oppRating = result.opponent.rating
+    val oppDeviation = result.opponent.rating
+
+    val gValue = gFunction(oppDeviation)
+    val eValue = eFunction(playerRating, oppRating, oppDeviation)
+
+    return gValue * (result.type.score - eValue)
+}
+
+fun eFunction(rating: Float, oppRating: Float, oppDeviation: Float): Float {
 
     return (1 + exp(-1 * gFunction(oppDeviation) * (rating - oppRating))).pow(-1)
 }
@@ -43,3 +46,4 @@ fun gFunction(deviation: Float): Float {
 
     return (1 + 3 * deviation.pow(2) / PI).toFloat().pow(-1)
 }
+
