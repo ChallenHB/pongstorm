786f0bb369e6eb0a65a5d741a5de50ffe9c4224c
Challen Herzberg-Brovold <crnhb38@gmail.com>
2018-12-14 19:13:35 -0600

    Just saving stuff

--- src/main/kotlin/com/headstorm/pongstorm/rating/glicko2/Glicko2Functions.kt
+++ src/main/kotlin/com/headstorm/pongstorm/rating/glicko2/Glicko2Functions.kt
@@ -2,6 +2,7 @@ package com.headstorm.pongstorm.rating.glicko2
 
 import kotlin.math.PI
 import kotlin.math.exp
+import kotlin.math.ln
 import kotlin.math.pow
 
 val epsilon = 0.000001
@@ -9,13 +10,11 @@ val epsilon = 0.000001
 // This function will take glicko2 scores, not glicko scores
 fun calculatePlayerScore(player: Player, results: List<Result>): Player {
 
-
     val v = results.map{ vSumFunction(player, it) }.reduce{ acc, it -> acc + it }.pow(-1)
     val delta = results.map{ deltaSumFunction(player, it) }.reduce{ acc, it -> acc + it }.times(v)
     return Player(123, 1f, 1f, 1f)
 }
 
-// Function to be called in summation
 fun vSumFunction(player: Player, result: Result): Float {
     val playerRating = player.rating
     val oppRating = result.opponent.rating
@@ -38,7 +37,19 @@ fun deltaSumFunction(player: Player, result: Result): Float {
     return gValue * (result.type.score - eValue)
 }
 
-fun volatilityIterativeFn(deviation: Float, v: Float, delta: Float, tao: Float): (Float, Float) -> Float = { x, a ->
+fun calculateDeviation(volatility: Float, deviation: Float, v: Float, delta: Float, tao: Float): Float {
+    val fx = createIterativeDeviationFn(deviation, v, delta, tao)
+    val a = ln(volatility.pow(2))
+    val d2 = delta.pow(2)
+    val b = when {
+        d2 > deviation.pow(2) + v -> ln(d2 - deviation.pow(2) - v)
+        else -> 
+    }
+
+    return 1f
+}
+
+fun createIterativeDeviationFn(deviation: Float, v: Float, delta: Float, tao: Float): (Float, Float) -> Float = { x, a ->
     val num1 = exp(x) * (delta.pow(2) - deviation.pow(2) - v - exp(x))
     val den1 = 2 * (deviation.pow(2) + v + exp(x)).pow(2)
 
@@ -49,13 +60,10 @@ fun volatilityIterativeFn(deviation: Float, v: Float, delta: Float, tao: Float):
 }
 
 fun eFunction(rating: Float, oppRating: Float, oppDeviation: Float): Float {
-
     return (1 + exp(-1 * gFunction(oppDeviation) * (rating - oppRating))).pow(-1)
 }
 
 // Is it faster to 1/val or val.pow(-1)? (benchmarks)
 fun gFunction(deviation: Float): Float {
-
     return (1 + 3 * deviation.pow(2) / PI).toFloat().pow(-1)
 }
-
